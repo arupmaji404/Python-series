743aba97c0e37f92dd370f3278d5abd32bec60df
Arup Maji <arupmaji02017@gmail.com>
2020-06-10 11:07:37 +0530

    adding int and float

---
+++ Python-Task 1/int_and_float.py
@@ -0,0 +1,29 @@
+# working with integers
+
+#initializing the variables
+
+num1 = 10
+num2 = 5
+
+#calculating the arithmetic values and printing them in place
+
+print("Sum is ", num1 + num2)				# this will give interger result as num1 and num2 are integers
+print("Diff is ", num1 - num2)				# this will also give integers as output
+print("Product is ", num1 * num2)			# same here
+print("Division is ", num1 / num2)			# this will give float output because division is always float
+print()
+
+# working with float
+
+#initializing the variables
+
+num1 = 10.5
+num2 = 5.5
+
+#calculating the arithmetic values and printing them in place
+
+print("Sum is ", num1 + num2)				# this will give interger result as num1 and num2 are float
+print("Diff is ", num1 - num2)				# this will also give integers as output
+print("Product is ", num1 * num2)			# same here
+print("Division is ", num1 / num2)			# this always gives float as output
+
